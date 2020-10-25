b66a704bd711bf68eaca24c7bbf32449d2e5a71c
Erik Salino <esalino@supermoney.com>
2020-10-25 14:10:05 -0700

    Initial commit

---
+++ src/test/java/maps/MakeAnagramsTest.java
@@ -0,0 +1,15 @@
+package maps;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class MakeAnagramsTest {
+	
+	 @Test
+	 public void testMakeAnagrams() {
+		 final int result = MakeAnagrams.makeAnagrams("cde", "abc");
+		 assertEquals(4, result);
+    }
+
+}
